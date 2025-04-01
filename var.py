4495a8b6e1d3fff32968bd86f5f696096b499f2b
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-04-01 21:26:19 +0530

    learning python vars

---
+++ var.py
@@ -0,0 +1,11 @@
+#Created by: Jayachandra_kumar
+#Topic: python variables
+print("Hello World")
+a = 2
+b = 4
+res = a + b
+print("a =",a , "b =",b)
+print("a+b")
+print("result:", res)
+print("result Data_type:" , type(res))
+print("result Memory_address:",id(res))
